fe0558506c7634506cac02aa52d28becc9226c24
Domgee <asdfadwf@gmail.com>
2024-11-02 17:27:10 +0700

    :)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js
+
 const express = require("express");
 const app = express();
 
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 
-app.get("/*(", (req, res) => {
+app.get("/*", (req, res) => {
   
   res.json("Hello world");
 });
